8854ad78bab0063787b831d1d51dccd635bfdb76
niubrandon <niubrandon@gmail.com>
2022-01-01 13:09:21 -0500

    added server api

---
+++ Auth0ServerApi/Controllers/Api.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Auth0ServerApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet("public")]
+        public IActionResult Public()
+        {
+            Console.WriteLine("receiving call from front");
+            return Ok(new
+            {
+                Message = "Hello from a public endpoint! You don't need to be authenticated to see this."
+            });
+        }
+
+        [HttpGet("private")]
+        [Authorize]
+        public IActionResult Private()
+        {   
+            Console.WriteLine("receiving call from front for private");
+            return Ok(new
+            {
+                Message = "Hello from a private endpoint! You need to be authenticated to see this."
+            });
+        }
+
+        [HttpGet("private-scoped")]
+        [Authorize("read:actions")]
+        public IActionResult Scoped()
+        {
+            Console.WriteLine("receiving call from front for private-scoped");
+            return Ok(new
+            {
+                Action = "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this."
+            });
+        }
+
+        // This is a helper action. It allows you to easily view all the claims of the token.
+        [HttpGet("claims")]
+        public IActionResult Claims()
+        {
+            return Ok(User.Claims.Select(c =>
+                new
+                {
+                    c.Type,
+                    c.Value
+                }));
+        }
+    }
+}
+
---
+++ Auth0ServerApi/Program.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+
+string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => 
+{
+  options.AddPolicy(name: MyAllowSpecificOrigins,
+  builder =>
+  {
+    builder.WithOrigins("*").AllowAnyHeader().AllowAnyMethod();
+  });
+});
+//code below auth0 section
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+     .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, c =>
+     {
+         c.Authority = $"https://{builder.Configuration["Auth0:Domain"]}";
+         c.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+         {
+             ValidAudience = builder.Configuration["Auth0:Audience"],
+             ValidIssuer = $"{builder.Configuration["Auth0:Domain"]}"
+         };
+     });
+
+     builder.Services.AddAuthorization(o =>
+    {
+        o.AddPolicy("read:actions", p => p.
+            RequireAuthenticatedUser().
+            RequireClaim("scope", "read:actions"));
+    });
+// code above auth0 section
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors(MyAllowSpecificOrigins);
+
+app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//auth0 section below
+app.UseCors();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+//auth0 section above        
+
+    
+
+app.Run();
+
